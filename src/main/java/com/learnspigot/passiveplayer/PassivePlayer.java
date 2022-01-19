30aa960a7ad6c77c6a85a77bdcaaedb1b5eef2a6
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-01-19 06:25:02 +0200

    updated the package

--- src/main/java/com/learnspigot/passiveplayer/PassivePlayer.java
+++ src/main/java/com/learnspigot/passiveplayer/PassivePlayer.java
@@ -3,7 +3,7 @@ package com.learnspigot.passiveplayer;
 import com.learnspigot.passiveplayer.commands.PassiveCommand;
 import com.learnspigot.passiveplayer.listeners.JoinQuitListener;
 import com.learnspigot.passiveplayer.listeners.PvPListener;
-import com.learnspigot.smp.core.utils.commandframework.CommandFramework;
+import com.learnspigot.smp.core.util.commandframework.CommandFramework;
 import lombok.val;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
